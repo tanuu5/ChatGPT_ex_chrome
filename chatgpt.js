aa2813c4cd7e7eae80ae5dcfb3c56fad12b76de7
たぬ <134081795+tanuu5@users.noreply.github.com>
2023-08-31 12:38:04 +0900

    Add files via upload

--- chatgpt.js
+++ chatgpt.js
@@ -1,15 +1,19 @@
+// Retrieve history and counter from local storage
+var historyText = localStorage.getItem('history') || "";
+var counter = parseInt(localStorage.getItem('counter'), 10) || 0;
+
+// Display history and counter on the screen
+document.getElementById('history').innerText = historyText;
+document.getElementById('counter').innerText = "Counter: " + counter;
+
 document.getElementById('chat-form').addEventListener('submit', function(event) {
     event.preventDefault();
-
     var initialMessage = { 'role': 'system', 'content': '' };
     var userMessageContent = document.getElementById('message-input').value;
     var userMessage = { 'role': 'user', 'content': userMessageContent };
-
     sendToChatGPT([initialMessage, userMessage]);
-
-    // ユーザーのメッセージを表示
     var userMessageElem = document.createElement('p');
-    userMessageElem.textContent = "あなた: " + userMessageContent;
+    userMessageElem.innerHTML = "You: " + userMessageContent.replace(/\n/g, "<br>");
     document.getElementById('chat-log').appendChild(userMessageElem);
 });
 
@@ -23,14 +27,37 @@ function sendToChatGPT(messages) {
         xhr.setRequestHeader('Authorization', 'Bearer ' + apiKey);
 
         xhr.onreadystatechange = function() {
-            if (this.readyState === 4 && this.status === 200) {
-                var response = JSON.parse(this.responseText);
-                var chatGptMessageContent = response['choices'][0]['message']['content'];
+            if (this.readyState === 4) {
+                document.getElementById('loading-indicator').style.display = 'none';
 
-                // ChatGPTのメッセージを表示
-                var chatGptMessageElem = document.createElement('p');
-                chatGptMessageElem.textContent = "ChatGPT: " + chatGptMessageContent;
-                document.getElementById('chat-log').appendChild(chatGptMessageElem);
+                if (this.status === 200) {
+                    var response = JSON.parse(this.responseText);
+                    var chatGptMessageContent = response['choices'][0]['message']['content'];
+
+                    // Timestamp
+                    var timestamp = new Date().toLocaleString();
+
+                    // Update history with your prompt and ChatGPT's reply
+                    historyText += "---\n" + "You (" + timestamp + "): " + messages[messages.length - 1]['content'] + "\n";
+                    historyText += "ChatGPT (" + timestamp + "): " + chatGptMessageContent + "\n";
+
+                    counter++;
+
+                    // Save to local storage and display on screen
+                    document.getElementById('history').innerText = historyText;
+                    document.getElementById('counter').innerText = "Counter: " + counter;
+                    localStorage.setItem('history', historyText);
+                    localStorage.setItem('counter', counter);
+
+                    // Display ChatGPT's message
+                    var chatGptMessageElem = document.createElement('p');
+                    chatGptMessageElem.innerHTML = "ChatGPT: " + chatGptMessageContent.replace(/\n/g, "<br>");
+                    document.getElementById('chat-log').appendChild(chatGptMessageElem);
+                } else if (this.status === 401) {
+                    alert("APIキーが誤っています。");
+                } else {
+                    alert("エラーが発生しました。HTTPステータスコード: " + this.status);
+                }
             }
         };
 
@@ -42,3 +69,13 @@ function sendToChatGPT(messages) {
         xhr.send(JSON.stringify(data));
     });
 }
+
+// 履歴を表示・非表示を切り替えるボタン
+document.getElementById('toggle-history').addEventListener('click', function() {
+    var historySection = document.getElementById('history-section');
+    if (historySection.style.display === 'none') {
+        historySection.style.display = 'block';
+    } else {
+        historySection.style.display = 'none';
+    }
+});
--- options.js
+++ options.js
@@ -1,13 +1,74 @@
-document.getElementById('settings-form').addEventListener('submit', function(event) {
-    event.preventDefault();
+document.addEventListener('DOMContentLoaded', function() {
+    // Load and display the current counter and estimated cost
+    var counter = parseInt(localStorage.getItem('counter'), 10) || 0;
+    document.getElementById('current-counter').innerText = counter;
+    document.getElementById('estimated-cost').innerText = (counter * 0.002).toFixed(2);
 
-    var apiKey = document.getElementById('api-key-input').value;
-    chrome.storage.sync.set({ 'openaiApiKey': apiKey }, function() {
-        alert('設定は保存されました（Settings saved.）');
+    // Load API Key if available and mask it
+    chrome.storage.sync.get('openaiApiKey', function(data) {
+        if (data.openaiApiKey) {
+            document.getElementById('api-key-input').value = '●●●●●●●●';
+        }
     });
+
+    // API Key form submission
+    document.getElementById('settings-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        var apiKeyInput = document.getElementById('api-key-input');
+        
+        // Update only if a new value is entered
+        if (apiKeyInput.value !== '●●●●●●●●') {
+            chrome.storage.sync.set({openaiApiKey: apiKeyInput.value}, function() {
+                alert('API Key saved.');
+                apiKeyInput.value = '●●●●●●●●';
+            });
+        }
+    });
+
+    // Reset Counter
+    document.getElementById('reset-counter').addEventListener('click', function() {
+        localStorage.setItem('counter', 0);
+        updateCounterAndCost();
+        alert('Counter has been reset.');
+    });
+
+    // Reset History
+    document.getElementById('reset-history').addEventListener('click', function() {
+        localStorage.setItem('history', '');
+        alert('History has been reset.');
+    });
+
+    // Download History
+    document.getElementById('download-history').addEventListener('click', function() {
+        var history = localStorage.getItem('history') || '';
+        var blob = new Blob([history], { type: 'text/plain' });
+        var url = window.URL.createObjectURL(blob);
+
+        var a = document.createElement('a');
+        a.style.display = 'none';
+        a.href = url;
+        a.download = 'chat_history.txt';
+
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
+    });
+
+    // Set Counter Manually
+    document.getElementById('set-counter').addEventListener('click', function() {
+        var newCounter = parseInt(document.getElementById('manual-counter').value, 10);
+        if (isNaN(newCounter)) {
+            alert('Please enter a valid number.');
+            return;
+        }
+        localStorage.setItem('counter', newCounter);
+        updateCounterAndCost();
+    });
+
+    function updateCounterAndCost() {
+        var counter = parseInt(localStorage.getItem('counter'), 10) || 0;
+        document.getElementById('current-counter').innerText = counter;
+        document.getElementById('estimated-cost').innerText = (counter * 0.002).toFixed(2);
+    }
 });
 
-// Load any saved API key into the settings form
-chrome.storage.sync.get('openaiApiKey', function(data) {
-    document.getElementById('api-key-input').value = data.openaiApiKey;
-});
