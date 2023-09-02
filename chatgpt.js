3492078551c4075cf38ceb688e8fc785cb71c8d6
たぬ <134081795+tanuu5@users.noreply.github.com>
2023-09-02 21:41:10 +0900

    Add files via upload
    Add personal info detection, multi-language support and minor layout fixes

--- chatgpt.js
+++ chatgpt.js
@@ -1,23 +1,69 @@
-// Retrieve history and counter from local storage
 var historyText = localStorage.getItem('history') || "";
 var counter = parseInt(localStorage.getItem('counter'), 10) || 0;
+
+var alertMessages = {
+    en: {
+        detected: 'Detected personal information.\nDetected Items: \n',
+        proceed: 'Are you sure you want to proceed?(Press OK to send.)',
+        cancel: 'The message was not sent because a cancel was pressed.\nPlease mask or delete the relevant part and resubmit.',
+        apiKeyError: 'The API key is incorrect.',
+        otherError: 'An error occurred. HTTP Status Code: '
+    },
+    ja: {
+        detected: '個人情報が検出されました。\n検出された項目: \n',
+        proceed: '本当に送信してもよろしいですか？（送信するにはOKを押してください。）',
+        cancel: 'キャンセルが押されたため、メッセージは送信されませんでした。\n関連する部分をマスクまたは削除して、再送信してください。',
+        apiKeyError: 'APIキーが間違っています。',
+        otherError: 'エラーが発生しました。HTTPステータスコード: '
+    }
+};
 
 // Display history and counter on the screen
 document.getElementById('history').innerText = historyText;
 document.getElementById('counter').innerText = "Counter: " + counter;
 
-document.getElementById('chat-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var initialMessage = { 'role': 'system', 'content': '' };
-    var userMessageContent = document.getElementById('message-input').value;
-    var userMessage = { 'role': 'user', 'content': userMessageContent };
-    sendToChatGPT([initialMessage, userMessage]);
-    var userMessageElem = document.createElement('p');
-    userMessageElem.innerHTML = "You: " + userMessageContent.replace(/\n/g, "<br>");
-    document.getElementById('chat-log').appendChild(userMessageElem);
-});
+async function sendToChatGPT(messages) {
+    // 状態変数
+    let shouldProceed = true;
 
-function sendToChatGPT(messages) {
+    // 送信前に個人情報をチェック
+    const checkPersonalInfo = new Promise((resolve) => {
+    chrome.storage.sync.get(['enablePersonalInfoCheck', 'selectedLanguage'], function(data) {
+        var enableCheck = data.enablePersonalInfoCheck || false;
+        var language = data.selectedLanguage || 'en';
+        var msg = alertMessages[language];
+            if (enableCheck) {
+                var inputText = document.getElementById('message-input').value;
+                var phoneRegex = /\d{2,4}-\d{2,4}-\d{4}/g;
+                var emailRegex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g;
+                var postalRegex = /\d{3}-\d{4}/g;
+        
+                var foundPhone = inputText.match(phoneRegex);
+                var foundEmail = inputText.match(emailRegex);
+                var foundPostal = inputText.match(postalRegex);
+
+                if (foundPhone || foundEmail || foundPostal) {
+                var detectedItems = '';
+                if (foundPhone) detectedItems += 'Phone Numbers: ' + foundPhone.join(', ') + '\n';
+                if (foundEmail) detectedItems += 'Email Addresses: ' + foundEmail.join(', ') + '\n';
+                if (foundPostal) detectedItems += 'Postal Codes: ' + foundPostal.join(', ') + '\n';
+                
+                var proceed = window.confirm(msg.detected + detectedItems + msg.proceed);
+                if (!proceed) {
+                    shouldProceed = false;
+                    alert(msg.cancel);
+                }
+            }
+            }
+            resolve(shouldProceed); // Promiseをresolveして値を返す
+        });
+    });
+
+    shouldProceed = await checkPersonalInfo; // 非同期処理が完了するまで待つ
+
+    // shouldProceed が false なら後続の処理をスキップ
+    if (!shouldProceed) return;
+
     chrome.storage.sync.get('openaiApiKey', function(data) {
         var apiKey = data.openaiApiKey;
 
@@ -54,7 +100,7 @@ function sendToChatGPT(messages) {
                     chatGptMessageElem.innerHTML = "ChatGPT: " + chatGptMessageContent.replace(/\n/g, "<br>");
                     document.getElementById('chat-log').appendChild(chatGptMessageElem);
                 } else if (this.status === 401) {
-                    alert("APIキーが誤っています。");
+                    alert("The API key is incorrect.(APIキーが間違っています。)");
                 } else {
                     alert("エラーが発生しました。HTTPステータスコード: " + this.status);
                 }
@@ -78,4 +124,32 @@ document.getElementById('toggle-history').addEventListener('click', function() {
     } else {
         historySection.style.display = 'none';
     }
+});
+
+document.getElementById('chat-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var initialMessage = { 'role': 'system', 'content': '' };
+    var userMessageContent = document.getElementById('message-input').value;
+    var userMessage = { 'role': 'user', 'content': userMessageContent };
+    sendToChatGPT([initialMessage, userMessage]);
+    var userMessageElem = document.createElement('p');
+    userMessageElem.innerHTML = "You: " + userMessageContent.replace(/\n/g, "<br>");
+    document.getElementById('chat-log').appendChild(userMessageElem);
+});
+
+// Function to set the UI language in the popup
+function setPopupLanguage(language) {
+  const messageText1 = language === 'ja' ? '送信ボタンを押した後、返信がくるまで少々お待ちください...' : 'Please wait a moment after you hit send until you hear back... ';
+  const messageText2 = language === 'ja' ? '（メッセージが正常に受信された場合、このメッセージは消えます。）' : '(This message will disappear when the message is successfully received.)';
+  const sendButton = language === 'ja' ? '送信' : 'Send';
+  
+  document.getElementById('message-placeholder1').textContent = messageText1;
+  document.getElementById('message-placeholder2').textContent = messageText2;
+  document.getElementById('send-button').textContent = sendButton;
+}
+
+// Initialize language setting for the popup
+chrome.storage.sync.get('selectedLanguage', function(data) {
+  const savedLanguage = data.selectedLanguage || 'en';
+  setPopupLanguage(savedLanguage);
 });
--- options.js
+++ options.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Load and display the current counter and estimated cost
+        // Load and display the current counter and estimated cost
     var counter = parseInt(localStorage.getItem('counter'), 10) || 0;
     document.getElementById('current-counter').innerText = counter;
     document.getElementById('estimated-cost').innerText = (counter * 0.002).toFixed(2);
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Load API Key if available and mask it
     chrome.storage.sync.get('openaiApiKey', function(data) {
         if (data.openaiApiKey) {
-            document.getElementById('api-key-input').value = '●●●●●●●●';
+            document.getElementById('api-key-input').value = '●●●●●●●●●●●●●●●●';
         }
     });
 
@@ -17,10 +17,10 @@ document.addEventListener('DOMContentLoaded', function() {
         var apiKeyInput = document.getElementById('api-key-input');
         
         // Update only if a new value is entered
-        if (apiKeyInput.value !== '●●●●●●●●') {
+        if (apiKeyInput.value !== '●●●●●●●●●●●●●●●●') {
             chrome.storage.sync.set({openaiApiKey: apiKeyInput.value}, function() {
                 alert('API Key saved.');
-                apiKeyInput.value = '●●●●●●●●';
+                apiKeyInput.value = '●●●●●●●●●●●●●●●●';
             });
         }
     });
@@ -70,5 +70,92 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('current-counter').innerText = counter;
         document.getElementById('estimated-cost').innerText = (counter * 0.002).toFixed(2);
     }
-});
 
+    // Load the state of the personal information check feature
+    chrome.storage.sync.get('enablePersonalInfoCheck', function(data) {
+        var isChecked = data.enablePersonalInfoCheck || false;
+        document.getElementById('enablePersonalInfoCheck').checked = isChecked;
+    });
+
+    // Save the state of the personal information check feature when the checkbox is clicked
+    document.getElementById('enablePersonalInfoCheck').addEventListener('click', function() {
+        var isChecked = document.getElementById('enablePersonalInfoCheck').checked;
+        chrome.storage.sync.set({ 'enablePersonalInfoCheck': isChecked });
+    });
+
+    // Function to set the UI language
+    function setLanguage(language) {
+    const settingsTitle = language === 'ja' ? 'GPT-ChatDIY 設定' : 'GPT-ChatDIY Settings';
+    const apiKeyTitle = language === 'ja' ? 'API キー' : 'API Key';
+    const languageTitle = language === 'ja' ? '言語' : 'Language';
+    const apiKeyInstruction = language === 'ja' ? '以下にOpenAI APIキーを入力してください：' : 'Enter your OpenAI API key below:';
+    const saveButton = language === 'ja' ? '保存' : 'Save';
+    const counterAndCost = language === 'ja' ? 'カウンターとコスト' : 'Counter and Cost';
+    const currentCounterTitle = language === 'ja' ? '現在のカウンター：' : 'Current counter:';
+    const estimatedCostTitle = language === 'ja' ? '推定コスト：' : 'Estimated cost:';
+    const checkOpenAIPricing = language === 'ja' ? '正確な請求については、OpenAIの価格ページを確認してください。' : 'Check the OpenAI Pricing page for accurate billing.';
+    const setCounterManually = language === 'ja' ? '手動でカウンターを設定します：' : 'Set Counter Manually:';
+    const setCounter = language === 'ja' ? 'カウンターを設定する' : 'Set Counter';
+    const history = language === 'ja' ? '履歴' : 'History';
+    const downloadHistory = language === 'ja' ? '履歴のダウンロード' : 'Download History';
+    const reset = language === 'ja' ? 'リセット' : 'Reset';
+    const resetHistory = language === 'ja' ? '履歴のリセット' : 'Reset History';
+    const resetCounter = language === 'ja' ? 'カウンターリセット' : 'Reset Counter';
+    const personalInfoCheck = language === 'ja' ? '個人情報チェック' : 'Personal Information Check';
+    const enableDisableInfoCheck = language === 'ja' ? '個人情報チェック機能の有効・無効を設定します。' : 'Enable or disable the personal information check feature.';
+    const enableInfoCheckDetail = language === 'ja' ? '個人情報チェックを有効にする(電話番号、メールアドレス、郵便番号のみ検出)' : 'Enable Personal Information Check.(Only phone numbers, email addresses, and zip codes are detected.)';
+    
+    document.getElementById('settings-title').textContent = settingsTitle;
+    document.getElementById('api-key-title').textContent = apiKeyTitle;
+    document.getElementById('language-title').textContent = languageTitle;
+    document.getElementById('api-key-instruction').textContent = apiKeyInstruction;
+    document.getElementById('save-button').textContent = saveButton;
+    document.getElementById('counter-and-cost').textContent = counterAndCost;
+    document.getElementById('current-counter-title').textContent = currentCounterTitle;
+    document.getElementById('estimated-cost-title').textContent = estimatedCostTitle;
+    document.getElementById('check-openai-pricing').textContent = checkOpenAIPricing;
+    document.getElementById('set-counter-manually').textContent = setCounterManually;
+    document.getElementById('set-counter').textContent = setCounter;
+    document.getElementById('history').textContent = history;
+    document.getElementById('download-history').textContent = downloadHistory;
+    document.getElementById('reset').textContent = reset;
+    document.getElementById('reset-history').textContent = resetHistory;
+    document.getElementById('reset-counter').textContent = resetCounter;
+    document.getElementById('personal-info-check').textContent = personalInfoCheck;
+    document.getElementById('enable-disable-info-check').textContent = enableDisableInfoCheck;
+    document.getElementById('enable-info-check-detail').textContent = enableInfoCheckDetail;
+    }
+
+    // Event listener for language dropdown changes
+    const languageSelect = document.getElementById('language-select');
+    
+
+    // Initialize the dropdown with the currently selected language using chrome.storage.sync
+      chrome.storage.sync.get('selectedLanguage', function(data) {
+      const savedLanguage = data.selectedLanguage || 'en';
+      languageSelect.value = savedLanguage;
+      setLanguage(savedLanguage);
+    });
+    
+    // Update language when the dropdown selection changes
+    if (languageSelect) {
+      languageSelect.addEventListener('change', function() {
+        const selectedLanguage = this.value;
+        chrome.storage.sync.set({'selectedLanguage': selectedLanguage}, function() {
+          setLanguage(selectedLanguage);
+            });
+      });
+    }
+    
+    // デバッグ用 変数の値確認用
+    document.getElementById('show-storage').addEventListener('click', function() {
+        chrome.storage.sync.get(null, function(items) {
+            // オブジェクトをテキスト形式に変換
+            var storageText = JSON.stringify(items, null, 2);
+        
+            // テキストをHTMLに挿入
+            document.getElementById('storage-output').innerText = storageText;
+        });
+    });
+
+});
